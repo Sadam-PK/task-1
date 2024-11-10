1fae2702268a38503b5151155b7cc6d0d0ea61b5
sadam-PK <sadamccr@gmail.com>
2024-11-10 20:13:11 +0500

    web view completed

--- src/components/CustomInput.jsx
+++ src/components/CustomInput.jsx
@@ -1,6 +1,6 @@
 const CustomInput = ({ ...props }) => {
   return (
-    <div className="border rounded-lg w-[24vw] h-[6vh] outline-none">
+    <div className="border rounded-lg w-[24vw] h-[6vh] outline-none overflow-hidden">
       <input {...props} className="w-full h-full p-2 outline-none" />
     </div>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,38 +14,48 @@ const Login = () => {
          justify-around items-center gap-32"
       >
         <div className="flex flex-col w-[50%] mx-auto ">
-          <p>
-            <FontAwesomeIcon icon={faAngleLeft} /> Back
-          </p>
+          <a href="#" className="text-gray-400">
+            <FontAwesomeIcon icon={faAngleLeft} className="text-gray-400" />{" "}
+            Back
+          </a>
         </div>
 
-        <div className="flex flex-col items-start py-10">
-          <h2 className=" text-[36px]">Sign In</h2>
-          <p className="text-[16px]">
+        <div className="flex flex-col items-start">
+          <h2 className=" text-[36px] font-bold text-red-500">Sign In</h2>
+          <p className="text-[16px] text-gray-500">
             Enter your email and password to sign in!
           </p>
-          <div className="flex flex-row items-center w-[100%]">
-            <div className="flex-1 bg-gray-500 h-[1px]" />
-            <span className="p-2">or</span>
-            <div className="flex-1 bg-gray-500 h-[1px]" />
+          <div className="flex flex-row items-center w-[100%] text-gray-400">
+            <div className="flex-1 bg-gray-300 h-[1px]" />
+            <span className="px-2 py-4">or</span>
+            <div className="flex-1 bg-gray-300 h-[1px]" />
           </div>
-          <label htmlFor="">Email*</label>
+          <label htmlFor="" className="pb-2">
+            Email<span className="text-red-500">*</span>{" "}
+          </label>
           <CustomInput placeholder="mail@simmmple.com" />
-          <label htmlFor="">Password*</label>
+          <label htmlFor="" className="pt-4 pb-2">
+            Password<span className="text-red-500">*</span>{" "}
+          </label>
           <CustomInput placeholder="Min. 8 characters" />
-          <div className="flex flex-row ">
-            <label htmlFor="">
-              <input type="checkbox" />
-              keep me logged in
+          {/* &&&&&&&& check gap or justify &&&&&&& */}
+          <div className="flex flex-row py-6 gap-10">
+            <label htmlFor="" className="">
+              <input type="checkbox" className="mr-3" />
+              Keep me logged in
             </label>
-            <a href="#">forget password?</a>
+            <a href="#" className="text-red-500">
+              Forget password?
+            </a>
           </div>
-          <button className="bg-red-500 w-[24vw] h-[6vh] text-white rounded-lg">
+          <button className="bg-red-500 w-[24vw] h-[6vh] text-white rounded-xl font-bold">
             Sign In
           </button>
-          <div className="flex flex-row">
-            not registered yet?
-            <a href="#">Create an Account</a>
+          <div className="flex flex-row py-6 gap-1">
+            <p>Not registered yet?</p>
+            <a href="#" className="text-red-500">
+              Create an Account
+            </a>
           </div>
         </div>
 
@@ -62,9 +72,10 @@ const Login = () => {
           borderBottomLeftRadius: "110px",
         }}
       >
+        {/* height div */}
         <div />
         <div
-          className="text-center rounded-3xl flex flex-col 
+          className="text-center rounded-3xl flex flex-col pt-20
           justify-center items-center"
         >
           <div>
@@ -72,8 +83,9 @@ const Login = () => {
           </div>
           <div className="h-20" />
           <div
-            className="text-center border w-[28vw] h-[16vh] flex flex-col justify-center
-            items-center rounded-3xl"
+            className="text-center w-[28vw] h-[16vh] flex flex-col justify-center
+            items-center rounded-3xl ml-10"
+            style={{ border: "2px solid rgba(255, 255, 255, 0.2)" }}
           >
             <p className="text-white font-light">
               Learn more about Air Drive on{" "}
